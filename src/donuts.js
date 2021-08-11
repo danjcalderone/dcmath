17bb9aece4d139a0794ce8704cbb8022de621ce8
Daniel Calderone <dan@Daniels-MacBook-Air.local>
2021-08-11 08:26:32 -0700

    donut.js update

--- src/donuts.js
+++ src/donuts.js
@@ -1,30 +1,26 @@
-var xx = math.ones(2);
-
 Pts.namespace(window);
-var space2 = new CanvasSpace("#donut").setup({
-  bgcolor: "#0c3",
+var space = new CanvasSpace("donut").setup({
+  bgcolor: "rgba(0,0,0,0)",
   resize: true,
   retina: true
 });
-var form2 = space2.getForm();
-//// Demo code starts (anonymous function wrapper is optional) ---
+var form = space.getForm();
+//console.log("run donut");
 
-// (function () {
-
-space2.add({
+space.add({
   animate: (time, ftime) => {
-    let c1 = Circle.fromCenter(space2.pointer, space2.size.y / 4);
-    let c2 = Circle.fromCenter(space2.pointer, space2.size.y / 8);
-    let ct = Circle.fromCenter(space2.center, space2.size.y / 4);
+    let c1 = Circle.fromCenter(space.pointer, space.size.y / 4);
+    let c2 = Circle.fromCenter(space.pointer, space.size.y / 8);
+    let ct = Circle.fromCenter(space.center, space.size.y / 4);
 
     let ins1 = Circle.intersectCircle2D(c1, ct);
     let ins2 = Circle.intersectCircle2D(c2, ct);
 
-    form2.fillOnly("#0c6").circle(c1);
-    form2.fill("#fe3").circle(c2);
-    form2.fill("rgba(70,30,240,.2)").circle(ct);
-    form2.fill("rgba(70,30,240, .3)").points(ins1, 10, "circle");
-    form2.fill("#f06").points(ins2, 5, "circle");
+    form.fillOnly("#0c6").circle(c1);
+    form.fill("#fe3").circle(c2);
+    form.fill("rgba(70,30,240,.2)").circle(ct);
+    form.fill("rgba(70,30,240, .3)").points(ins1, 10, "circle");
+    form.fill("#f06").points(ins2, 5, "circle");
   }
 });
-space2.bindMouse().bindTouch().play();
+space.bindMouse().bindTouch().play();
