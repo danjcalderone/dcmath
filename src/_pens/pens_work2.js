b8d82564265715619f00ec684691558a641441b9
Daniel Calderone <dan@Daniels-MacBook-Air.local>
2021-08-19 17:42:57 -0700

    removing src

--- src/_pens/pens_work2.js
+++
@@ -1,6 +0,0 @@
-export var pens = {
-  pen0: { sz: 10, lw: 2, lrgba: "rgba(0,0,0,0.7)", frgba: "rgba(0,0,0,0.1)" },
-  pen1: { sz: 10, lw: 2, lrgba: "rgba(0,0,0,0.7)", frgba: "rgba(0,0,0,0.1)" },
-  pen2: { sz: 10, lw: 2, lrgba: "rgba(0,0,0,0.7)", frgba: "rgba(0,0,0,0.1)" },
-  pen3: { sz: 10, lw: 2, lrgba: "rgba(0,0,0,0.7)", frgba: "rgba(0,0,0,0.1)" }
-};
