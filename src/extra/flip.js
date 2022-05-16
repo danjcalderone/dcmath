6d0d370619b85f7008639fd57578714b91987686
Daniel Calderone <dan@Daniels-MacBook-Air.local>
2022-05-16 00:54:49 -0700

    more

--- src/extra/flip.js
+++ src/extra/flip.js
@@ -139,19 +139,18 @@ function addSlideDecks() {
 
 
 function showSlides(name,n) {
-  let i;
   let slides = document.getElementsByClassName(name);
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {slideIndexes[name] = 1}
+  if (n < 1) {slideIndexes[name] = slides.length}
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndexes[name]-1].style.display = "block";
+  dots[slideIndexes[name]-1].className += " active";
 }
 
 
