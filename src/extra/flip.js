5b0f994198b9fa870c42b2e05bad81d90eb5a54b
Daniel Calderone <dan@Daniels-MacBook-Air.local>
2022-05-15 17:15:20 -0700

    more

--- src/extra/flip.js
+++ src/extra/flip.js
@@ -4,19 +4,20 @@
 
 
 function addSlides(path,maxNum) {
-  let slideDecks = document.getElementById("slide-container1");
-  var node = slideDecks.createElement('p');
+  // let slideDecks = document.getElementById("slide-container1");
+  // var node = slideDecks.createElement('p');
   for (let i = 0; i < maxNum; i++) {
     // var newNode = document.createElement('p');
     var htmlText = 'yo'
-  //     //
+    var newNode = document.createElement('p');
+    var textNode = document.createTextNode(htmlText);
+    newNode.append(textNode);
+    document.getElementById('slide-container1').appendChild(newNode)
   //     // "<div class="mySlides fade">
   //     //   <div class="numbertext">1 / 3</div>
   //     //   <img src="./img1.png" style="width:100%">
   //     // <div class="text">Caption Text</div>
   //     // </div>"
-      newNode.appendChild(document.createTextNode(htmlText));
-      node.appendChild(newNode);
     }
   }
 
