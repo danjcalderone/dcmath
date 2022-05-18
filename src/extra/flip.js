e210bf03635f200db108dddf2ff290344f3671e4
Daniel Calderone <dan@Daniels-MacBook-Air.local>
2022-05-17 18:49:08 -0700

    more

--- src/extra/flip.js
+++ src/extra/flip.js
@@ -21,85 +21,91 @@
 //     }
 //   }
 
-function addSlides(name,maxNum){
 
-var path = '/dcmath/figs/flips/' + name + '/' + name + '_'
+var name = 'KALMAN1024'
+var maxNum = 'maxNum9';
+// let slideDecks = document.getElementById("slide-container1");
+// var node = slideDecks.createElement('p');
 
-for (let i = 1; i < maxNum; i++) {
-  // var newNode = document.createElement('p');
-  // var htmlText = 'yo'
-  // var newNode = document.createElement('p');
-  // var frag = document.createDocumentFragment();
-  var newFig = document.createElement('div');
-  // newFig.classList.add("mySlides","fade")
-  newFig.classList.add(name)
+function addSlide(name){
+    // let slideDecks = document.getElementById("slide-container1");
+    // var node = slideDecks.createElement('p');
 
-  // document.getElementById('slide-container1').appendChild(newFig)
-  var newNumText = document.createElement('div')
-  var newImg = document.createElement('img')
-  var newCaption = document.createElement('div')
-  newNumText.classList.add("numbertext")
-  newCaption.classList.add('text')
+    // var maxNum = 'maxNum9';
+
+    var path = '/dcmath/figs/flips/' + name + '/' + name + '_'
+    // document.getElementById("id1").getAttribute("any");
+    var maxNum = document.getElementById(name).getAttribute("any");
+
+    for (let i = 1; i < maxNum; i++) {
+      // var newNode = document.createElement('p');
+      // var htmlText = 'yo'
+      // var newNode = document.createElement('p');
+      // var frag = document.createDocumentFragment();
+      var newFig = document.createElement('div');
+      // newFig.classList.add("mySlides","fade")
+      newFig.classList.add(name)
+
+      // document.getElementById('slide-container1').appendChild(newFig)
+      var newNumText = document.createElement('div')
+      var newImg = document.createElement('img')
+      var newCaption = document.createElement('div')
+      newNumText.classList.add("numbertext")
+      newCaption.classList.add('text')
 
 
-  // <div class="mySlides fade">
-  //   <div class="numbertext">1 / 3</div>
-  //   <img src="./img1.png" style="width:100%">
-  //   <div class="text">Caption Text</div>
-  // </div>
-  //
-  // newNumText.textContent= String(i) +' / ' + String(maxNum)
-  newImg.src = path + String(i) + '.png'
-  newImg.style = "width:100%"
-  newNumText.textContent= '1 / 5'
-  newCaption.textContent='Caption Text'
-  newFig.append(newNumText)
-  newFig.append(newImg)
-  newFig.append(newCaption)
-  document.getElementById(name).appendChild(newFig)
-  // document.getElementById('slide-container1').appendChild(newNumText)
-  // document.getElementById('slide-container1').appendChild(newImg)
-  // document.getElementById('slide-container1').appendChild(newCaption)
+      // <div class="mySlides fade">
+      //   <div class="numbertext">1 / 3</div>
+      //   <img src="./img1.png" style="width:100%">
+      //   <div class="text">Caption Text</div>
+      // </div>
+      //
+      // newNumText.textContent= String(i) +' / ' + String(maxNum)
+      newImg.src = path + String(i) + '.png'
+      newImg.style = "width:100%"
+      newNumText.textContent= '1 / 5'
+      newCaption.textContent='Caption Text'
+      newFig.append(newNumText)
+      newFig.append(newImg)
+      newFig.append(newCaption)
+      document.getElementById(name).appendChild(newFig)
+      // document.getElementById('slide-container1').appendChild(newNumText)
+      // document.getElementById('slide-container1').appendChild(newImg)
+      // document.getElementById('slide-container1').appendChild(newCaption)
 
-  //
-  // var textNode = document.createTextNode(htmlText);
-  // newNode.append(textNode);
-  // document.getElementById('slide-container1').appendChild(newNode)
-//     // "<div class="mySlides fade">
-//     //   <div class="numbertext">1 / 3</div>
-//     //   <img src="./img1.png" style="width:100%">
-//     // <div class="text">Caption Text</div>
-//     // </div>"
+      //
+      // var textNode = document.createTextNode(htmlText);
+      // newNode.append(textNode);
+      // document.getElementById('slide-container1').appendChild(newNode)
+    //     // "<div class="mySlides fade">
+    //     //   <div class="numbertext">1 / 3</div>
+    //     //   <img src="./img1.png" style="width:100%">
+    //     // <div class="text">Caption Text</div>
+    //     // </div>"
+    }
+
+    var prevButton = document.createElement('a')
+    var nextButton = document.createElement('a')
+
+    prevButton.class="prev"
+    prevButton.classList.add("prev")
+    var onClickString = "plusSlides('"+name+"',-1)"
+    prevButton.setAttribute('onclick',onClickString)
+    prevButton.innerHTML = "&#10094;"
+
+    nextButton.class="next"
+    nextButton.classList.add("next")
+    onClickString = "plusSlides('"+name+"',1)"
+    nextButton.setAttribute('onclick',onClickString)
+    nextButton.innerHTML = "&#10095;"
+
+    document.getElementById(name).appendChild(prevButton)
+    document.getElementById(name).appendChild(nextButton)
+
+
 
 }
 
-var prevButton = document.createElement('a')
-var nextButton = document.createElement('a')
-
-prevButton.class="prev"
-prevButton.classList.add("prev")
-prevButton.setAttribute('onclick',"plusSlides(-1)")
-prevButton.innerHTML = "&#10094;"
-
-nextButton.class="next"
-nextButton.classList.add("next")
-nextButton.setAttribute('onclick',"plusSlides(1)")
-nextButton.innerHTML = "&#10095;"
-
-document.getElementById(name).appendChild(prevButton)
-document.getElementById(name).appendChild(nextButton)
-
-
-
-
-// <div style="text-align:center">
-//   <span class="dot" onclick="currentSlide(1)"></span>
-//   <span class="dot" onclick="currentSlide(2)"></span>
-//   <span class="dot" onclick="currentSlide(3)"></span>
-// </div>
-
-
-}
 
 
 
@@ -167,6 +173,11 @@ function plusSlides(name,n) {
 function currentSlide(name,n) {
   showSlides(name,slideIndexes[name] = n);
 }
+
+
+
+addSlide(name); //,parseInt(maxNum[6
+showSlides(name,slideIndexes[name]);
 
 
 //
