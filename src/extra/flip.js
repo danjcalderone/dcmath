0a929da35d62374b03cbb9d6dcdbe937be407e82
Daniel Calderone <dan@Daniels-MacBook-Air.local>
2022-07-15 01:14:30 -0700

    more

--- src/extra/flip.js
+++ src/extra/flip.js
@@ -156,6 +156,8 @@ function showSlides(name,n,slideIndexes) {
   if (n < 1) {slideIndexes[name] = slides.length}
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slides[i].style.border-style = 'solid';
+    slides[i].style.border-width = '5px';
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
